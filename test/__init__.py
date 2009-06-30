6cc0ff16e9eebf4ed4cca252bb03b8f374996eed
Olmo Maldonado <olmo.maldonado@gmail.com>
2009-06-29 23:30:06 -0700

    Moving the logic to a separate module. It'll be easier on intra-package importing

---
+++ test/__init__.py
@@ -0,0 +1,15 @@
+import sys, os; sys.path.append(os.pardir)
+from Cametrics import Cametrics 
+
+# Simple Unit Test for Cametrics
+Cametrics.initialize('ahBjYW1ldHJpY3Mtc3RyZXNzchALEghDYW1wYWlnbhjCgAIM', {
+    'url.host': 'cametrics-stress.appspot.com'
+})
+
+print Cametrics.getURL()
+print Cametrics.getOptions()
+print Cametrics.getSecretKey()
+Cametrics.measure('contributions.names', 'joe', 'string')
+Cametrics.measure('contributions.locations', {'lng': -118.01, 'lat':34.22},'location')
+Cametrics.measure('contributions.score', 4, 'number')
+Cametrics.measure('contributions.time', '2009-08-17 13:12:01', 'datetime')
